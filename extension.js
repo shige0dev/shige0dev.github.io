a224c90e40000e5a23036f30e9041da284b88954
shige0dev@gmail.com <33574014+shige0dev@users.noreply.github.com>
2018-03-13 23:30:54 +0900

    2

--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
 (function(ext) {
+    var win = window.open('http://www.google.com','subWindow','width=320,height=320');
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -9,13 +10,13 @@
     };
 
     ext.open_window = function(location) {
-        var win = window.open('http://www.google.com','subWindow','width=320,height=320');
+        //var win = window.open('http://www.google.com','subWindow','width=320,height=320');
         return win;
     };
 
     ext.close_window = function(w){
-        console.log(w);
-        w.close();
+        console.log(win);
+        win.close();
     };
 
     // Block and block menu descriptions
@@ -23,6 +24,7 @@
         blocks: [
             ['r', 'open window %s', 'open_window', 'Boston'],
             ['w', 'close window %s', 'close_window', 'window'],
+            ['w', 'resize window %s', 'resize_window', 'window'],
         ]
     };
 
